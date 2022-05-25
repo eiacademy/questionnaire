e032d8aa33e620886c4ce956b4517d0012c62c0d
Kevin Chu <kvnchu2@gmail.com>
2022-05-25 16:59:13 -0700

    add nextClick function and surveyAnswers and pageAnswers state

--- src/components/Survey/index.js
+++ src/components/Survey/index.js
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 export function Survey() {
 
     const [ questionNumber, setQuestionNumber ] = useState(0);
+    //these are the answers collected per page
+    const [ pageAnswers, setPageAnswers ] = useState([]); 
+    //these are the accumulated answers from pageAnswers to be submitted to the database
+    const [ surveyAnswers, setSurveyAnswers ] = useState([]);
 
     //to be converted to a JSON file eventually
     const questionnaireQuestions = {
@@ -12,16 +16,26 @@ export function Survey() {
             },
             {
                 question: "Do you have any siblings?"
+            },
+            {
+                question: "Do you play sports?"
             }
         ]
+    }
+
+    //triggers when you click next button. if answers have been selected, it will set surveyAnswers with pageAnswers and move on to the next page
+    const nextClick = () => {
+        if (pageAnswers.length !== 0) {
+            setSurveyAnswers(prevState => [...prevState, pageAnswers]);
+            setQuestionNumber(prevState => prevState + 1)
+        }
     }
 
     return (
         <div className="container">
             <label>{questionnaireQuestions.questions[questionNumber]["question"]}</label>
-            <button onClick={() => setQuestionNumber(prevState => prevState - 1)}>Previous</button>
-            <button onClick={() => setQuestionNumber(prevState => prevState + 1)}>Next</button>
+            <input onChange={event => {setPageAnswers(event.target.value)}}></input>
+            <button onClick={() => nextClick()}>Next</button>
         </div>
     );
 }
-  
