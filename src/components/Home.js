f185c900cf4fee449008173d7e297defe6e5d2b4
Kevin Chu <kvnchu2@gmail.com>
2022-05-28 22:29:03 -0700

    redo state to accommodate for branching in the future and add value/name to input fields

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,5 +13,5 @@ export function HomePage() {
             <Navbar></Navbar>
         </div>
     );
-  }
+}
   
--- src/components/Survey/index.js
+++ src/components/Survey/index.js
@@ -3,39 +3,69 @@ import React, { useState } from "react";
 export function Survey() {
 
     const [ questionNumber, setQuestionNumber ] = useState(0);
-    //these are the answers collected per page
-    const [ pageAnswers, setPageAnswers ] = useState([]); 
-    //these are the accumulated answers from pageAnswers to be submitted to the database
-    const [ surveyAnswers, setSurveyAnswers ] = useState([]);
+
+    const [ state, setState ] = useState({
+        fruits: "",
+        fruitsType: "",
+        siblings: "",
+        siblingsNumber: "",
+        sports: "",
+        sportsType: ""
+    })
+
 
     //to be converted to a JSON file eventually
     const questionnaireQuestions = {
         questions: [
             {
-                question: "Do you like fruits?"
+                title: "fruits",
+                question: "Do you like fruits?",
+                followUpQuestions:{
+                    title: "fruitsType",
+                    question:"What type of fruit do you like?"
+                }
+            },
+            {   
+                title: "siblings",
+                question: "Do you have any siblings?",
+                followUpQuestions:{
+                    title: "siblingsNumber",
+                    question: "How many siblings do you have?"
+                }
             },
             {
-                question: "Do you have any siblings?"
-            },
-            {
-                question: "Do you play sports?"
+                title: "sports",
+                question: "Do you play sports?",
+                followUpQuestions:{
+                    title:"sportsType",
+                    question:"What sports do you play?"
+                }
             }
         ]
     }
 
+    const onChange = e => {
+        const { name, value } = e.target;
+        setState(prevState => ({ ...prevState, [name]: value }));
+      };
+
     //triggers when you click next button. if answers have been selected, it will set surveyAnswers with pageAnswers and move on to the next page
     const nextClick = () => {
-        if (pageAnswers.length !== 0) {
-            setSurveyAnswers(prevState => [...prevState, pageAnswers]);
-            setQuestionNumber(prevState => prevState + 1)
-        }
+        setQuestionNumber(prevState => prevState + 1)
     }
 
     return (
         <div className="container">
             <label>{questionnaireQuestions.questions[questionNumber]["question"]}</label>
-            <input onChange={event => {setPageAnswers(event.target.value)}}></input>
+            <input value={state[questionnaireQuestions.questions[questionNumber]["title"]]} name={questionnaireQuestions.questions[questionNumber]["title"]} onChange={onChange}></input>
+            <input value={state[questionnaireQuestions.questions[questionNumber]["followUpQuestions"]["title"]]} name={questionnaireQuestions.questions[questionNumber]["followUpQuestions"]["title"]} onChange={onChange}></input>
             <button onClick={() => nextClick()}>Next</button>
+            {state.fruits}
+            {state.fruitsType}
+            {state.siblings}
+            {state.siblingsNumber}
+            {state.sports}
+            {state.sportsType}
         </div>
     );
 }
